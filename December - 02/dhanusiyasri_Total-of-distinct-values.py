8cdf8ed44529662af2f666495ec4bb79b2e42169
dhanusiyasri <msdhanusiya@gmail.com>
2025-12-05 23:30:01 +0530

    added solution 2

--- December - 02/dhanusiyasri_Total-of-distinct-values.py
+++ December - 02/dhanusiyasri_Total-of-distinct-values.py
@@ -1,9 +1,10 @@
-n = int(input())
-width = len(bin(n)) - 2
-for i in range(1, n + 1):
+def total_of_distinct_values(n):
+    width = len(bin(n)) - 2
+    for i in range(1, n + 1):
         dec = str(i)
         octal = oct(i)[2:]        
         hexa = hex(i)[2:].upper() 
         binary = bin(i)[2:]       
         print(f"{dec:>{width}} {octal:>{width}} {hexa:>{width}} {binary:>{width}}")
-
+n = int(input())
+total_of_distinct_values(n)
