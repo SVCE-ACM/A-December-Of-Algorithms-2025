bc604cd3100bcd7f22c608eb90277d484a53f269
ananyascodehq <ananyakannan1502@gmail.com>
2025-12-09 21:17:36 +0530

    December - 02 completed

---
+++ December - 02/ananyascodehq_total-of-distint-values.java
@@ -0,0 +1,17 @@
+class Solution{
+    public void printTable(int N){
+        int width = Integer.toBinaryString(N).length();
+        for(int i = 1; i <=N; i++){
+            String decString = Integer.toString(i);
+            String octalString = Integer.toOctalString(i);
+            String hexString = Integer.toHexString(i);
+            String bString = Integer.toBinaryString(i);
+            System.out.printf("%" + width + "s %" + width + "s %" + width + "s %" + width + "s\n",decString, octalString, hexString, bString);
+        }
+    }
+
+     public static void main(String[] args) {
+        Solution s = new Solution();
+        s.printTable(6); // test input
+    }
+}
