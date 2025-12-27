67e4c4fc7a882cce998d733ba11eb514c48151c1
devipriya-l <jaidevipriya09@gmail.com>
2025-12-27 14:02:06 +0530

    Completed December 09 - Sum of Unique Elements

---
+++ December - 09/SumOfUniqueElements.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class SumOfUniqueElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        HashMap<Long, Integer> freqMap = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            long num = sc.nextLong();
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        long sum = 0;
+        for (Map.Entry<Long, Integer> entry : freqMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                sum += entry.getKey();
+            }
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
