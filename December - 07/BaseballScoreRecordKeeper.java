66127b4d3a76a8caf9d42c4ff8ad1bcaa1618fe8
devipriya-l <jaidevipriya09@gmail.com>
2025-12-27 13:50:18 +0530

    Completed December 07 - Baseball Score Record Keeper

---
+++ December - 07/BaseballScoreRecordKeeper.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class BaseballScoreRecordKeeper {
+
+    public static void main(String[] args) {
+        String[] ops = {"5","2","C","D","+"};
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : ops) {
+            if (op.equals("C")) {
+                stack.pop();
+            } 
+            else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } 
+            else if (op.equals("+")) {
+                int top = stack.pop();
+                int newScore = top + stack.peek();
+                stack.push(top);
+                stack.push(newScore);
+            } 
+            else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+        int total = 0;
+        for (int score : stack) {
+            total += score;
+        }
+        System.out.println(total);
+    }
+}
