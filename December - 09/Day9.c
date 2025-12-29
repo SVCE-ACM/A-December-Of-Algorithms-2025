1c3f49bdcc933c58fd8beb9bde64eac32aae400f
Tanushribalaji <btanushri6@gmail.com>
2025-12-29 13:54:57 +0530

    Add files via upload

---
+++ December - 09/Day9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j])
+                count++;
+        }
+
+        if (count == 1)
+            sum += arr[i];
+    }
+
+    printf("%lld\n", sum);
+
+    return 0;
+}
