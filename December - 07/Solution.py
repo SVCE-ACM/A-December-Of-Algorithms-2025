78f2d7ed3b98d99c5775ea8eb72ac51bcc294e64
Kaviarasi-Projects <kaviarasi.m.1807@gmail.com>
2025-12-30 18:23:49 +0530

    Completed Day-07

---
+++ December - 07/Solution.py
@@ -0,0 +1,14 @@
+def Solution(ops):
+    stack=[]
+    for i in ops:
+        if i=="C":
+            stack.pop()
+        elif i=="D":
+            stack.append(2*stack[-1])
+        elif i=="+":
+            stack.append(stack[-1]+stack[-2])
+        else:
+            stack.append(int(i))
+    return sum(stack)
+ops=input().split()
+print(Solution(ops))
