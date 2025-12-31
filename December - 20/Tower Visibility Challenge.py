36b9d327bc7a681cd9e8353e3eb7463504c5f0aa
Sree Ayishvarya D <ishuindira1981@gmail.com>
2025-12-31 14:37:27 +0530

    Tower Visibility Challenge.py

---
+++ December - 20/Tower Visibility Challenge.py
@@ -0,0 +1,14 @@
+N = int(input())
+heights = list(map(int, input().split()))
+
+result = [-1] * N
+stack = []
+
+for i in range(N-1, -1, -1):
+    while stack and stack[-1] <= heights[i]:
+        stack.pop()
+    if stack:
+        result[i] = stack[-1]
+    stack.append(heights[i])
+
+print(*result)
